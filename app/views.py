c229cf54f6c1d0aabfd4dbbe2d69266ceae33d56
Bartek <kowalczuk.bartek@gmail.com>
2019-05-11 19:22:43 +0200

    Add firebase connection

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
 from app import app
-from flask import Flask
+from flask import (Flask, render_template)
+import pyrebase
+import pandas as pd
+
+from fireConnect import fireConnect
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+
+    fc = fireConnect()
+    numActive,numUnique = fc.getStats()
+
+    return render_template('base.html',
+                           numActive=numActive,
+                           numUnique=numUnique,
+                           tables=fc.df.to_html(classes=["table-responsive"]))
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
---
+++ fireConnect.py
@@ -0,0 +1,29 @@
+import pyrebase
+import pandas as pd
+
+
+class fireConnect():
+
+    def __init__(self):
+        config = {
+        "apiKey": "AIzaSyCb4oRMVM6s4eFTJ9oMU5p4Xpv_9GQvMCE",
+        "authDomain": "studentgo-76a0d.firebaseapp.com",
+        "databaseURL": "https://studentgo-76a0d.firebaseio.com/",
+        "storageBucket": "studentgo-76a0d.appspot.com"
+        }
+
+        firebase = pyrebase.initialize_app(config)
+        auth = firebase.auth()
+        db = firebase.database()
+
+        values = db.child('Users').get()
+        data = pd.DataFrame(values.val())
+        self.df = data.transpose()
+
+    def getStats(self):
+        numActive = self.df.active[self.df.active == True].count()
+        numUnique = self.df.index.nunique()
+        return numActive, numUnique
+
+fc = fireConnect()
+i,j=fc.getStats()
